4caf23190a52724c6254caa3aa3bf99e1622ccce
judang <45169270+judangs@users.noreply.github.com>
2024-04-12 14:36:39 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 64.79 ms, Memory: 12.9 MB -BaekjoonHub

--- 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.cpp
+++ 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.cpp
@@ -14,34 +14,35 @@ vector<int> solution(vector<string> gems) {
 
     map<string, int> showcase;
     pair<int, int> width;
-    int start = 0, end, min_len = INT32_MAX;
-    for(end = 0; end<gems.size(); end++) {
+    int start = 0, end = start, min_len = INT32_MAX;
+    while(start <= end && end < gems.size()) {
         showcase[gems[end]]++;
-        if(showcase.size() == total.size())
-            break;
-    }
 
-    while(end < gems.size()) {
+        if(showcase.size() == total.size()) {
+            while(showcase.size() == total.size()) {
 
-        if(end - start < min_len) {
-            width.first = start;
-            width.second = end;
-            min_len = (end - start);
-        }
+                if(end - start < min_len) {
+                    if(width.first == start && width.second == end)
+                        return {start + 1, end + 1};
+                    width.first = start;
+                    width.second = end;
+                    min_len = (end - start);
+                }
 
-        string find_key = gems[start];
-        showcase[find_key]--;
-        
-        if(showcase[find_key] == 0) {
-            for(end += 1; end<gems.size(); end++) {
-                showcase[gems[end]]++;
-                if(gems[end] == find_key)
-                    break;
+                showcase[gems[start]]--;
+                if(showcase[gems[start]] == 0) {
+                    showcase.erase(showcase.find(gems[start]));
+                }
+                start++;
             }
-        }
 
-        start++;
+            start = end;
+            showcase.clear();
+        }
+        else
+            end++;
     }
 
-    return {width.first + 1, width.second + 1};
+    vector<int> answer = {width.first + 1, width.second + 1};
+    return answer;
 }
