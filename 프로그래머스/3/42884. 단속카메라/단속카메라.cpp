fa8ed7eab21138ead556fcf96ecdd5ff70937ab2
judang <45169270+judangs@users.noreply.github.com>
2024-04-23 21:59:28 +0900

    [level 3] Title: 단속카메라, Time: 3.76 ms, Memory: 3.99 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.cpp
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.cpp
@@ -1,25 +1,31 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <stack>
 #include <iostream>
 
 using namespace std;
 
 bool compare(vector<int> a, vector<int> b) {
-    return a[1] < b[1];
+    return a[0] < b[0];
 }
 
 int solution(vector<vector<int>> routes) {
     sort(routes.begin(), routes.end(), compare);
 
-    int current = INT32_MIN;
-    int answer = 0;
-    for(auto route: routes) {
-        if(current < route[0]) {
-            answer++;
-            current = route[1];
+    stack<pair<int, int>> st;
+    st.push(make_pair(routes.front()[0], routes.front()[1]));
+    
+    for(int i=1; i<routes.size(); i++) {
+        if(st.top().second < routes[i][0]) {
+            st.push(make_pair(routes[i][0], routes[i][1]));
+        }
+        else {
+            st.top().first = routes[i][0];
+            st.top().second = min(st.top().second, routes[i][1]);
         }
     }
 
+    int answer = st.size();
     return answer;
 }
