63568b5af2bd559e2d2ae06b252a194a89601fcc
judang <45169270+judangs@users.noreply.github.com>
2024-07-14 14:59:42 +0900

    [level 2] Title: 기능개발, Time: 0.01 ms, Memory: 4.22 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.cpp
+++ 프로그래머스/2/42586. 기능개발/기능개발.cpp
@@ -1,35 +1,32 @@
-#include <string>
-#include <vector>
 #include <iostream>
+#include <vector>
+#include <queue>
 
 using namespace std;
 
+typedef pair<int, int> pii;
+
+queue<pii> q;
+
 vector<int> solution(vector<int> progresses, vector<int> speeds) {
+    for(int current=0; current < progresses.size(); current++) {
+        int day = (100 - progresses[current]) / speeds[current];
+        if((100 - progresses[current]) % speeds[current] != 0)
+            day++;
+        q.push({current, day});
+    }
+
     vector<int> answer;
-    
-    vector<int> days(speeds.size(), 0);
-    
-    for(int i=0; i<progresses.size(); i++) {
-        int remain = (100 - progresses[i]) / speeds[i];
-        
-        if((100 - progresses[i]) % speeds[i] > 0) remain++;
-        if(remain == 0) remain = 1;
-        days[i] = remain;
-    }
-    
-    days.push_back(999);
-    
-    for(int i=0; i<days.size()-1; i++) {
-        int count = 1;
-        int ptr = i + 1;
-        while(days[i] >= days[ptr]) {
-            ptr++;
-            count++;
+    while(!q.empty()) {
+        int deploy = 0;
+        int comp = q.front().second;
+        while(!q.empty() && q.front().second <= comp) {
+            deploy++;
+            q.pop();
         }
-        i = ptr - 1;
         
-        answer.push_back(count);
+        answer.push_back(deploy);
     }
-    
+
     return answer;
 }
