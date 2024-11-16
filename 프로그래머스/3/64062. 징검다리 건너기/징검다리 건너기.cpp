f3b2d005227222be2ee44a5ca23b645b313c0532
judang <45169270+judangs@users.noreply.github.com>
2024-11-16 23:54:55 +0900

    [level 3] Title: 징검다리 건너기, Time: 3.05 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.cpp
+++ 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.cpp
@@ -1,32 +1,41 @@
 #include <string>
 #include <vector>
-#include <queue>
+#include <algorithm>
 #include <iostream>
 
 using namespace std;
 
-struct compare {
-    bool operator()(pair<int, int> a, pair<int, int> b) {
-        return a.second < b.second;
-    }
-};
+bool possible(vector<int> & stones, int n, int k) {
+    int count = 0;
+    for(int i=0; i<stones.size(); i++) {
+        if(stones[i] - n < 0) {
+            count++;
+        }
+        else {
+            count = 0;
+        }
 
-priority_queue<pair<int, int>, vector<pair<int, int>>, compare> pq;
+        if(count >= k)
+            return false;
+    }
+
+    return true;
+}
 
 int solution(vector<int> stones, int k) {
-    for(int i=0; i<k; i++) {
-        pq.push(make_pair(i, stones[i]));
-    }
-
-    int answer = pq.top().second;
-    int end = k;
-    while(end < stones.size()) {
-        pq.push(make_pair(end, stones[end]));
-        while(pq.top().first < (end - k + 1)) {
-            pq.pop();
+    int start = 0;
+    int end = 1e9;
+    int answer = 0;
+    
+    while(start + 1 < end) {
+        int mid = (start + end) / 2;
+        if(possible(stones, mid, k)) {
+            start = mid;
+            answer = max(answer, start);
         }
-        end++;
-        answer = min(answer, pq.top().second);
+        else {
+            end = mid;
+        }
     }
 
     return answer;
