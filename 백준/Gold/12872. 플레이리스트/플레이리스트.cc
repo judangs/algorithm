559945b3d5503e3770417a6bdb1f012c63bdab9c
judang <45169270+judangs@users.noreply.github.com>
2025-03-17 15:52:32 +0900

    [Gold I] Title: 플레이리스트, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/12872. 플레이리스트/플레이리스트.cc
+++ 백준/Gold/12872. 플레이리스트/플레이리스트.cc
@@ -13,13 +13,15 @@ int main() {
     cin >> N >> M >> P;
     dp.resize(P + 1, vector<long long>(N + 1 ,0));
 
-    dp[0][0] = 1;
-    for(int pick=1; pick<=P; pick++) {
-        for(int type=0; type<=N; type++) {
-            if(type > 0) dp[pick][type] = (dp[pick][type] + dp[pick - 1][type - 1] * (N - type + 1)) % DIV;
-            if(type > M) dp[pick][type] = (dp[pick][type] + dp[pick - 1][type] * (type - M)) % DIV;
+    dp[1][1] = N;
+    for(int pick=2; pick<=P; pick++) {
+        for(int type=1; type<=N; type++) {
+            dp[pick][type] += (dp[pick - 1][type - 1] * (N - type + 1) % DIV);
+            if(type >= M) {
+                dp[pick][type] += (dp[pick - 1][type] * (type - M) % DIV);
+            }
         }     
     }
 
-    cout << dp[P][N] % DIV;
+    cout << (dp[P][N] % DIV);
 }
