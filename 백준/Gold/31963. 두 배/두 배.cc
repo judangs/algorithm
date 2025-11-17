db7ec06d9eeffee8e900de7a3e965177cecf0259
judang <45169270+judangs@users.noreply.github.com>
2025-11-17 23:11:13 +0900

    [Gold III] Title: 두 배, Time: 24 ms, Memory: 3976 KB, Score: 33 point -BaekjoonHub

--- 백준/Gold/31963. 두 배/두 배.cc
+++ 백준/Gold/31963. 두 배/두 배.cc
@@ -1,43 +1,35 @@
 #include <iostream>
 #include <vector>
-#include <cmath>
 
 #define fastio ios::sync_with_stdio(false); cin.tie(NULL);
 
 using namespace std;
 
 int N;
-vector<int> arr;
-
-bool valid(int a, int b) {
-    return (a <= b);
-}
+vector<long long> arr;
 
 int main() {
-
     fastio;
     cin >> N;
-    arr = vector<int>(N);
+    arr = vector<long long>(N);
     for(auto & A: arr) cin >> A;
 
     int answer = 0;
     for(int i = 1; i < N; i++) {
         if(arr[i - 1] > arr[i]) {
-            int lo = 1, hi = 30;
-            while(lo + 1 < hi) {
-                int mid = (lo + hi) / 2;
-                if(valid(arr[i - 1], pow(2, mid) * arr[i])) hi = mid;
-                else lo = mid;
+            int p = 0;
+            long long current = arr[i];
+            
+            while(current < arr[i - 1]) {
+                current *= 2;
+                p++;
             }
-
-            int p;
-            if(valid(arr[i - 1], pow(2, lo) * arr[i])) p = lo;
-            else p = hi;
-            arr[i] *= pow(2, p);
+            
+            arr[i] = current;
             answer += p;
         }
     }
 
-    cout << answer;
+    cout << answer << '\n';
     
 }
