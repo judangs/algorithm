76abecfb49e8c2e4f87c541b401f3da4d59bfd8d
judang <45169270+judangs@users.noreply.github.com>
2025-12-07 18:12:45 +0900

    [Gold III] Title: 부산 관광, Time: 64 ms, Memory: 3780 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/34118. 부산 관광/부산 관광.cc
+++ 백준/Gold/34118. 부산 관광/부산 관광.cc
@@ -8,77 +8,50 @@ using namespace std;
 int N;
 string A, B;
 
-const int inf = 1e9;
+const int INF = 1e9;
 vector<int> price(4);
 
-int skip[4] = { 1, 3, 5, 4 };
-int cache[2001][2001];
 
-int tour(int han, int jung) {
+int dp[2001][6][6][5];
 
-    if(han >= N && jung >= N) return 0;
+int solve(int day, int remA, int remB, int remP) {
+    if (day == N) return 0;
 
-    int & ret = cache[han][jung];
-    if(ret != -1) return ret;
-    
-    ret = inf;
-    
-    if(han < N && jung < N) {
-        bool needA = (A[han] == '1');
-        bool needB = (B[jung] == '1');
-        
-        if(!needA && !needB) {
-            ret = min(ret, tour(han + 1, jung + 1));
-        }
-        else if(needA && needB) {
+    int &ret = dp[day][remA][remB][remP];
+    if (ret != -1) return ret;
+    ret = INF;
 
-            if(han == jung) {
-                ret = min(ret, tour(han + 4, jung + 4) + price[3]);
-            }
-            
-            for(int i = 0; i < 3; i++) {
-                for(int j = 0; j < 3; j++) {
-                    ret = min(ret, tour(han + skip[i], jung + skip[j]) + price[i] + price[j]);
-                }
-            }
-        }
-        else if(needA && !needB) {
+    bool needA = (A[day] == '1');
+    bool needB = (B[day] == '1');
 
-            if(han == jung) {
-                ret = min(ret, tour(han + 4, jung + 4) + price[3]);
-            }
+    int lenA[4]  = {0, 1, 3, 5};
+    int lenB[4]  = {0, 1, 3, 5};
+    int lenP[2]  = {0, 4};
 
-            for(int i = 0; i < 3; i++) {
-                ret = min(ret, tour(han + skip[i], jung + 1) + price[i]);
-            }
-        }
-        else if(!needA && needB) {
+    int costA[4] = {0, price[0], price[1], price[2]};
+    int costB[4] = {0, price[0], price[1], price[2]};
+    int costP[2] = {0, price[3]};
 
-            if(han == jung) {
-                ret = min(ret, tour(han + 4, jung + 4) + price[3]);
-            }
 
-            for(int i = 0; i < 3; i++) {
-                ret = min(ret, tour(han + 1, jung + skip[i]) + price[i]);
+    for (int ia = 0; ia < 4; ++ia) {
+        for (int ib = 0; ib < 4; ++ib) {
+            for (int ip = 0; ip < 2; ++ip) {
+                int cost = costA[ia] + costB[ib] + costP[ip];
+
+                int nRemA = max(remA, lenA[ia]);
+                int nRemB = max(remB, lenB[ib]);
+                int nRemP = max(remP, lenP[ip]);
+
+                bool coverA = !needA || (nRemA > 0 || nRemP > 0);
+                bool coverB = !needB || (nRemB > 0 || nRemP > 0);
+                if (!coverA || !coverB) continue;
+
+                int afterA = (nRemA > 0 ? nRemA - 1 : 0);
+                int afterB = (nRemB > 0 ? nRemB - 1 : 0);
+                int afterP = (nRemP > 0 ? nRemP - 1 : 0);
+
+                ret = min(ret, cost + solve(day + 1, afterA, afterB, afterP));
             }
-        }
-    }
-    else if(han < N) {
-        if(A[han] == '1') {
-            for(int i = 0; i < 3; i++) {
-                ret = min(ret, tour(han + skip[i], jung) + price[i]);
-            }
-        } else {
-            ret = min(ret, tour(han + 1, jung));
-        }
-    }
-    else if(jung < N) {
-        if(B[jung] == '1') {
-            for(int i = 0; i < 3; i++) {
-                ret = min(ret, tour(han, jung + skip[i]) + price[i]);
-            }
-        } else {
-            ret = min(ret, tour(han, jung + 1));
         }
     }
 
@@ -86,13 +59,16 @@ int tour(int han, int jung) {
 }
 
 int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
     cin >> N;
     cin >> A >> B;
-    for(int & p: price) cin >> p;
+    for (int & p : price) cin >> p;
 
-    memset(cache, -1, sizeof(cache));
+    memset(dp, -1, sizeof(dp));
 
-    int answer = tour(0, 0);
-    cout << answer << endl;
+    int answer = solve(0, 0, 0, 0);
+    cout << answer << '\n';
 
 }
