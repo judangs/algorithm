9c26b6b22278acea2101eaaab69b3586a635f485
judang <45169270+judangs@users.noreply.github.com>
2026-03-23 15:48:39 +0900

    [Silver I] Title: 꿈틀꿈틀 호석 애벌레 - 기능성, Time: 0 ms, Memory: 2020 KB, Score: 36 point -BaekjoonHub

---
+++ 백준/Silver/20167. 꿈틀꿈틀 호석 애벌레 － 기능성/꿈틀꿈틀 호석 애벌레 － 기능성.cc
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <vector>
+
+#define fastio ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+using namespace std;
+
+const int inf = 1e9;
+int N, K;
+vector<int> foods;
+
+struct Node {
+    int end;
+    int cost;
+};
+vector<Node> arr;
+vector<int> cache;
+
+int dp(int depth) {
+    if(depth == N) return 0;
+    
+    auto & ret = cache[depth];
+    if(ret != -1) return ret;
+    
+    ret = 0;
+    ret = max(ret, dp(depth + 1));
+    ret = max(ret, dp(arr[depth].end + 1) + arr[depth].cost);
+    return ret;
+}
+
+int main() {
+
+    fastio;
+    cin >> N >> K;
+    foods = vector<int>(N);
+    arr = vector<Node>(N);
+    cache = vector<int>(N, -1);
+    for(auto & food: foods) cin >> food;
+
+    for(int i = 0; i < N; i++) {
+        int cost = 0, j;
+        for(j = i; j < N; j++) {
+            cost += foods[j];
+            if(K <= cost) break;
+        }
+        arr[i] = { j, (0 < cost - K ? cost - K : 0)};
+    }
+
+    int ans = dp(0);
+    cout << ans << "\n";
+
+}
