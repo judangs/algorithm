5fca82e21767965d8b728dd8ed1004ac11f2221f
judang <45169270+judangs@users.noreply.github.com>
2026-03-24 16:35:59 +0900

    [Platinum V] Title: 광부 호석, Time: 64 ms, Memory: 16364 KB, Score: 11 point -BaekjoonHub

--- 백준/Platinum/21279. 광부 호석/광부 호석.cc
+++ 백준/Platinum/21279. 광부 호석/광부 호석.cc
@@ -1,72 +1,60 @@
 #include <iostream>
 #include <vector>
-#include <queue>
-#include <algorithm>
 
 #define fastio ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define MAXLEN 100000
 
 using namespace std;
 typedef long long ll;
 
-int N;
-ll C;
+int N; ll C;
 
-struct Node {
-    int x;
-    int y;
-    ll value;
-};
+int main() {
 
-struct GreaterCompare {
-    bool operator()(const Node & l, const Node & r) {
-        if(l.x == r.x)
-            return l.y < r.y;
-        return l.x > r.x;
-    }
-};
+    fastio;
 
-struct LessCompare {
-    bool operator()(const Node & l, const Node & r) {
-        if(l.y == r.y)
-            return l.x > r.x;
-        return l.y < r.y;
-    }
-};    
-
-    int main() {
-
-        fastio;
-
-        priority_queue<Node, vector<Node>, GreaterCompare> xq;
-        priority_queue<Node, vector<Node>, LessCompare> yq;
-        
-        cin >> N >> C;
-        for(int i = 0; i < N; i++) {
-            int X, Y; ll V; cin >> X >> Y >> V;
-            xq.push({ X, Y, V }); yq.push({ X, Y, V});
-        }
-
-        ll ans = 0ll, now = 0ll; int count = 0;
-        int H = 100001, W = -1;
-        while(0 <= H && W <= 100000) {
+    vector<pair<int, ll>> xgadget[MAXLEN + 1];
+    vector<pair<int, ll>> ygadget[MAXLEN + 1];
     
-            if(count < C) {
-                now += xq.top().value; count++;
-                xq.pop();
-                W = (xq.empty() ? 100000 + 1 : xq.top().x);
-            }
-            
-            else if(count >= C) {
-                now -= yq.top().value; 
-                yq.pop();
-                H = (yq.empty() ? -1 : yq.top().y);
-                count--;
-            }
-
-            ans = max(ans, now);
-
-        }
-        
-        cout << ans << '\n';
-
+    cin >> N >> C;
+    for(int i = 0; i < N; i++) {
+        int X, Y; ll V; cin >> X >> Y >> V;
+        xgadget[X].push_back({ Y, V }); ygadget[Y].push_back({ X, V });
     }
+
+    int H = MAXLEN, W = -1;
+    int count = 0;
+    ll ans = 0ll, now = 0ll;
+    while(0 <= H && W < MAXLEN) {
+
+        int i;
+        if(count < C) {
+
+            W++;
+
+            for(int i = 0; i < xgadget[W].size(); i++) {
+                if(xgadget[W][i].first <= H) {
+                    now += xgadget[W][i].second;
+                    count++;
+                }
+            }
+
+            if(count <= C)
+                ans = max(ans, now);
+        }
+        else {
+            for(int i = 0; i < ygadget[H].size(); i++) {
+                if(ygadget[H][i].first <= W) {
+                    now -= ygadget[H][i].second;
+                    count--;
+                }
+            }
+
+            H--;
+        }
+    }
+
+    cout << ans << '\n';
+    
+
+}
